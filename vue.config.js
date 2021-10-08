137b6382751b4320c2513b8af750bf4ce3dd981d
Ivan Che <ivan.chepurin@gmail.com>
2021-10-08 23:11:39 +0300

    public path

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,10 @@
 module.exports = {
+  publicPath: '',
   chainWebpack: config => {
     const svgRule = config.module.rule('svg')
 
-    // clear all existing loaders.
-    // if you don't do this, the loader below will be appended to
-    // existing loaders of the rule.
     svgRule.uses.clear()
 
-    // add replacement loader(s)
     svgRule
       .use('url-loader')
         .loader('url-loader')
