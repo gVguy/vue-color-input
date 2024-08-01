4d4122b9552de6da9b4c54cf25d7004c0fca2fef
Ivan Che <ivan.chepurin@gmail.com>
2024-08-01 15:56:24 +0200

    update version to 2.0.0, delete old build config

--- dev/serve.js
+++ dev/serve.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import Dev from './serve.vue';
 import DevSetup from './serve-setup.vue';
 
-const app = createApp(DevSetup);
+const app = createApp(Dev);
 app.config.unwrapInjectedRef = true;
 app.mount('#app');
 
