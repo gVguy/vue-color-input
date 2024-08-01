b85822c6963a0c6823f55f30180f0e6219476839
Ivan Che <ivan.chepurin@gmail.com>
2024-08-01 20:37:17 +0200

    update demo base dir

--- vite.config-demo.js
+++ vite.config-demo.js
@@ -1,6 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 /** @type {import('vite').UserConfig} */
 export default {
+  base: '/vue-color-input/',
   build: {
     outDir: './demo',
     emptyOutDir: true
